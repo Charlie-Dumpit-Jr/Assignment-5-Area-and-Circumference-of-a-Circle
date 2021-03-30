55a0024b2f5060b8a503b233dcb64a9eef72678c
CharlieDumpitJr <charlie.jr.dumpit@ocsbstudent.ca>
2021-03-30 23:08:20 +0000

    Assignment #5: Area and Circumference of a Circle

--- script.js
+++ script.js
@@ -8,10 +8,11 @@ document.getElementById('button').addEventListener('click', calculate)
 function calculate () {
   number = document.getElementById('input').value
   number = parseInt(number)
-  area = .25 * number ** 2 * PI 
+  PI = PI
+  area = 0.25 * number ** 2 * PI
   circumference = PI * number
   document.getElementById('answer1').innerHTML = 'Area = ' + area
-  document.getElementById('Therefore2').innerHTML = 'Therefore, the Area of the Circle is ' + area
+  document.getElementById('therefore2').innerHTML = 'Therefore, the Area of the Circle is ' + area
   document.getElementById('answer2').innerHTML = 'Circumference = ' + circumference
-  document.getElementById('Therefore1').innerHTML = 'The Circumference of the Circle is ' + circumference
+  document.getElementById('therefore1').innerHTML = 'The Circumference of the Circle is ' + circumference
 }
